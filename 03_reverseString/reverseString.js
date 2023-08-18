2efab2332d684ae469b77492a8d17088c52f7d32
DiepvriesRisotto <95715044+DiepvriesRisotto@users.noreply.github.com>
2023-08-18 17:41:47 +0200

    Finish exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = string.split("");
+    let reverseArray = stringArray.reverse();
+    let arrayString = reverseArray.join("");
+    return arrayString;
 };
 
 // Do not edit below this line
